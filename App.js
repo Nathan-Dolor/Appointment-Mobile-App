069f7d5f996aa9f4cf07d2b45656be9dfef0d7f6
Nathan Dolor <natebdolor@gmail.com>
2023-04-29 13:50:27 -0400

    Minor changes

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import BookAppointmentScreen from './screens/BookAppointmentScreen';
 import ViewAppointmentsScreen from './screens/ViewAppointmentsScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ViewAllAppointmentsScreen from './screens/ViewAllAppointmentsScreen';
 
 const Stack = createNativeStackNavigator();
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Book" component={BookAppointmentScreen} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
@@ -31,20 +19,12 @@ export default function App() {
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-        <Stack.Screen name="BookAppointment" component={BookAppointmentScreen} />
-        <Stack.Screen name="ViewAppointments" component={ViewAppointmentsScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="BookAppointment" component={BookAppointmentScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="ViewAppointments" component={ViewAppointmentsScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="ViewAllAppointments" component={ViewAllAppointmentsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-  // return (
-  
-  //   <NavigationContainer>
-  //   <Stack.Navigator>
-  //   <Stack.Screen options={{ headerShown: false }} name="Onboarding" component={OnboardingScreen} />
-  //   </Stack.Navigator>
-  //     <MyTabs />
-  //   </NavigationContainer>
-  // );
 }
 
 const styles = StyleSheet.create({
